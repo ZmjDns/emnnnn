f37cb1abadc28c17339e8930d102dfc0414cde80
ZmjDns <292570927@qq.com>
2020-10-30 18:06:49 +0800

    Natjs include contains problem

---
+++ src/pages/aboutNat/Medai.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <navi-header title="Nat相关"></navi-header>
+
+        <div>
+            <WxcButton class="" text="打开相册" @wxcButtonClicked="openGallery"></WxcButton>
+        </div>
+
+        <div>
+            <text>info: {{info}}</text>
+        </div>
+
+    </div>
+</template>
+<script>
+import { WxcButton } from 'weex-ui'
+import NaviHeader from '@/components/NaviHeader'
+import Nat from 'natjs'
+
+export default {
+    components: { NaviHeader, WxcButton, },
+    data () {
+        return {
+            info: ''
+        }
+    },
+    methods: {
+        openGallery () {
+            let that = this
+            Nat.image.pick({
+                limit: 3,
+                showCamera: false
+            }, (err, result) => {
+                console.log(result.paths)
+                that.info = JSON.stringify(result)
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
